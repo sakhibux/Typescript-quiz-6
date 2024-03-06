bec446c92dff507c98b207750b535b616cf45cfb
sakhi baksh <161965468+sakhibux@users.noreply.github.com>
2024-03-06 21:21:53 +0500

    Add files via upload

---
+++ bat.js
@@ -0,0 +1,8 @@
+// Quiz 6
+// const myName: string = "sakhi bux";
+// \t used for tab/space
+var myName = "\tsakhi bux";
+// \n used for new line
+var myName = "\n sakhi baksh";
+// console.log(myName);
+console.log(myName.trim());
---
+++ bat.ts
@@ -0,0 +1,12 @@
+// Quiz 6
+
+// const myName: string = "sakhi bux";
+// \t used for tab/space
+const myName: string = "\tsakhi bux";
+// \n used for new line
+const myName: string = "\n sakhi baksh";
+
+// console.log(myName);
+
+
+console.log(myName.trim());
